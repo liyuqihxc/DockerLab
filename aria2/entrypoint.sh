eef6c390327ac8a8d5728223e854191a4e2cb814
liyuqihxc <liyuqihxc@gmail.com>
2020-01-13 21:38:07 +0800

    aria2

--- aria2/entrypoint.sh
+++ aria2/entrypoint.sh
@@ -2,4 +2,11 @@
 
 touch /aria2/aria2.session
 darkhttpd /aria2/AriaNg --port 6801 &
-aria2c --conf-path=/aria2/aria2.conf
+
+while true; do
+  list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
+  sed -i "s@bt-tracker.*@bt-tracker=$list@g" /aria2/aria2.conf
+  [[ $(jobs | wc -l) == "2" ]] && kill %2
+  aria2c --conf-path=/aria2/aria2.conf &
+  sleep 2h
+done
