f783121190d6fba5d9ca49c89d8972771bc821c6
liyuqihxc <392036904@qq.com>
2020-05-11 14:40:16 +0800

    aria2

--- aria2/entrypoint.sh
+++ aria2/entrypoint.sh
@@ -7,15 +7,22 @@ PID_HTTPD=$!
 PID_ARIA2=""
 
 while true; do
+  echo "Updating trackers list...."
   list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt|awk NF|sed ":a;N;s/\n/,/g;ta"`
-  sed -i "s@bt-tracker.*@bt-tracker=$list@g" /aria2/aria2.conf
+  sed -i "s@bt-tracker.*@bt-tracker=$list@g" /aria2/data/aria2.conf
   if [ -n "$list" ];then
     [ -n "$PID_ARIA2" ] && kill "$PID_ARIA2"
-    aria2c --conf-path=/aria2/aria2.conf --stop-with-process="$PID_HTTPD" &
+    aria2c --conf-path=/aria2/data/aria2.conf --stop-with-process="$PID_HTTPD" &
     PID_ARIA2=$!
-    echo "New trackers List: "
+    echo "New trackers list: "
     echo "$list"
     echo ""
+  else
+    echo "Failed to update trackers list."
   fi
+  [ -n "$PID_ARIA2" ] && kill "$PID_ARIA2"
+  aria2c --conf-path=/aria2/data/aria2.conf --stop-with-process="$PID_HTTPD" &
+  PID_ARIA2=$!
+  echo "aria2c is running...."
   sleep 12h
 done
