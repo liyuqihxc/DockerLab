a732d98e2455a9aaa511d178f21652b1929aa4dd
liyuqihxc <liyuqihxc@gmail.com>
2020-06-23 00:30:08 +0800

    aria2

--- aria2/entrypoint.sh
+++ aria2/entrypoint.sh
@@ -8,12 +8,11 @@ show_help() {
   echo "Options:"
   echo "  -s, --rpc-secret       RPC授权令牌"
   echo "  -a, --address          Upnp映射地址"
-  echo "  -p, --port             Upnp映射端口"
   echo "  -h, --help             显示帮助信息并退出"
   echo ""
   echo "Example:"
   echo "    docker run [docker_opts] liyuqihxc/aria2[:tag] -s <super secret token>"
-  echo "    docker run [docker_opts] --network host liyuqihxc/aria2[:tag] -s <super secret token> -p 6881-6999"
+  echo "    docker run [docker_opts] --network host liyuqihxc/aria2[:tag] -s <super secret token> -a <ip address>"
   echo ""
   exit 0
 }
@@ -36,10 +35,6 @@ do
       UPNP_ADDRESS="$2"
       shift
       ;;
-    -p|--port)
-      UPNP_PORTS="$2"
-      shift
-      ;;
     -h|--help)
       show_help
       ;;
@@ -53,22 +48,23 @@ do
 done
 
 if [ -n "$RPC_SECRET" ];then
-  sed -i "s/^rpc-secret.*$/rpc-secret=$RPC_SECRET/g" /aria2/data/aria2.conf
+  sed -i "s/^rpc-secret.*$/rpc-secret=$RPC_SECRET/g" /aria2/aria2.conf
 else
-  sed -i "s/^rpc-secret.*$/#rpc-secret=/g" /aria2/data/aria2.conf
+  sed -i "s/^rpc-secret.*$/#rpc-secret=/g" /aria2/aria2.conf
 fi
 
-if [[ ( -n "$UPNP_ADDRESS" ) && ( -n "$UPNP_PORTS" ) ]];then
-  echo $UPNP_PORTS | sed -e "s/:/ /" | read start stop
-  for((i=$start;i<=$stop;i++));
+if [[ -n $UPNP_ADDRESS && -n $UPNP_PORTS ]];then
+  for i in $(seq $(echo $UPNP_PORTS | sed -e "s/-/ /"))
   do
+    echo "Mapping endpoint: $UPNP_ADDRESS:$i"
     upnpc -a $UPNP_ADDRESS $i $i TCP;
     upnpc -a $UPNP_ADDRESS $i $i UDP;
   done
 fi
 
-touch /aria2/data/aria2.session
-darkhttpd /aria2/AriaNg --port $HTTP_LISTEN_PORT &
+touch /aria2/config/aria2.session
+darkhttpd /aria2/AriaNg --port %HTTP_LISTEN_PORT% &
+cp /aria2/aria2.conf /aria2/config/aria2.conf
 
 PID_HTTPD=$!
 PID_ARIA2=""
@@ -76,11 +72,8 @@ PID_ARIA2=""
 while true; do
   echo "Updating trackers list...."
   list=`wget -qO- https://raw.githubusercontent.com/ngosang/trackerslist/master/trackers_best.txt | awk NF | sed ":a;N;s/\n/,/g;ta"`
-  sed -i "s/^bt-tracker.*$/bt-tracker=$list/g" /aria2/data/aria2.conf
+  sed -i "s/^bt-tracker.*$/bt-tracker=$list/g" /aria2/config/aria2.conf
   if [ -n "$list" ];then
-    [ -n "$PID_ARIA2" ] && kill "$PID_ARIA2"
-    aria2c --conf-path=/aria2/data/aria2.conf --stop-with-process="$PID_HTTPD" &
-    PID_ARIA2=$!
     echo "New trackers list: "
     echo "$list"
     echo ""
@@ -89,7 +82,7 @@ while true; do
   fi
   [ -n "$PID_ARIA2" ] && kill "$PID_ARIA2"
   PID_ARIA2=""
-  aria2c --conf-path=/aria2/data/aria2.conf --stop-with-process="$PID_HTTPD" &
+  aria2c --conf-path=/aria2/config/aria2.conf --stop-with-process="$PID_HTTPD" &
   PID_ARIA2=$!
   echo "aria2c is running...."
   sleep 12h
